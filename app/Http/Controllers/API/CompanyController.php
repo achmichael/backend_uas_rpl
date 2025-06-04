7b7abd978361affcebc5554d468c30d94445e4db
achmichael <achmadmichael4@gmail.com>
2025-06-04 08:44:14 +0700

    fix (CompanyController/showByUserId): modify ORM query to load nested relationship employee from employees

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -178,7 +178,7 @@ class CompanyController extends Controller
 
     public function showByUserId($userId)
     {
-        $company = Company::with(['user', 'employees'])->withCount('employees')
+        $company = Company::with(['user', 'employees.employee'])->withCount('employees')
             ->where('user_id', $userId)
             ->first();
             
